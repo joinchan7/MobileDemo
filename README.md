6c236fe130769d8d181784171da4979adba99a39
chan <a1477345842@outlook.com>
2019-09-18 20:51:09 +0800

    add left-nav

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mytest
+
 Just a test.
 2019.9.17 add myWeb.
--- js/bannner.js
+++ js/bannner.js
@@ -1,65 +1,63 @@
-function byId(id){
-    return typeof(id)==="string"?document.getElementById(id):id;
+function byId(id) {
+    return typeof id === "string" ? document.getElementById(id) : id;
 }
 
 // 全局变量
-var index=0,
-    timer=null,
-    pics=byId("banner-box").getElementsByTagName("div"),
-    dots=byId("dots").getElementsByTagName("span"),
-    prev=byId("prev"),
-    next=byId("next"),
-    len=pics.length;
-    
+var index = 0,
+    timer = null,
+    pics = byId("banner-box").getElementsByTagName("div"),
+    dots = byId("dots").getElementsByTagName("span"),
+    prev = byId("prev"),
+    next = byId("next"),
+    len = pics.length;
 
-function slideImg(){
-    var banner=byId("banner");
+function slideImg() {
+    var banner = byId("banner");
     // 滑过清除定时器,离开继续
-    banner.onmouseover=function(){
+    banner.onmouseover = function () {
         // 滑过清除定时器
-        if(timer) clearInterval(timer);
-    }
-    banner.onmouseout=function(){
-        timer=setInterval(function(){
+        if (timer) clearInterval(timer);
+    };
+    banner.onmouseout = function () {
+        timer = setInterval(function () {
             index++;
-            if(index>=len){
-                index=0;
+            if (index >= len) {
+                index = 0;
             }
             changeImg();
-        },3000);
-    }
+        }, 3000);
+    };
     //自动在banner上触发鼠标离开事件
     banner.onmouseout();
 
     //遍历所有点击,且绑定点击事件,点击圆点切换图片
-    for(var d=0;d<len;d++){
-        dots[d].id=d;
-        dots[d].onclick=function(){
+    for (var d = 0; d < len; d++) {
+        dots[d].id = d;
+        dots[d].onclick = function () {
             //改变index为当前span的索引
-            index=this.id;
+            index = this.id;
             changeImg();
-        }
+        };
     }
-    next.onclick=function(){
+    next.onclick = function () {
         index++;
-        if(index>=len)  index=0;
+        if (index >= len) index = 0;
         changeImg();
-    }
-    prev.onclick=function(){
+    };
+    prev.onclick = function () {
         index--;
-        if(index<0) index=len-1;
+        if (index < 0) index = len - 1;
         changeImg();
-    }
+    };
 }
 
 function changeImg() {
-    for(var i=0;i<len;i++){
-        pics[i].style.display="none";
-        dots[i].className="";
+    for (var i = 0; i < len; i++) {
+        pics[i].style.display = "none";
+        dots[i].className = "";
     }
-    pics[index].style.display="block";
-    dots[index].className="active";
+    pics[index].style.display = "block";
+    dots[index].className = "active";
 }
-
 
 slideImg();
